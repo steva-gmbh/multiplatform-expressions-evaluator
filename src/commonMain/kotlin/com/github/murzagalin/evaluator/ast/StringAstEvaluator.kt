0e44c9d1067b076b5fa22ee0749e7dc268e7c62d
Dirk Grappendorf <dg@steva.digital>
2024-02-16 21:46:58 +0100

    refactor: code clenaup

--- src/commonMain/kotlin/com/github/murzagalin/evaluator/ast/StringAstEvaluator.kt
+++ src/commonMain/kotlin/com/github/murzagalin/evaluator/ast/StringAstEvaluator.kt
@@ -5,7 +5,6 @@ internal class StringAstEvaluator {
     fun evaluate(expression: Expression, values: Map<String, Any> = emptyMap()): String {
         val baseEvaluator = AstEvaluator(values)
         val evaluated = baseEvaluator.evaluate(expression)
-
         return evaluated.toString()
     }
 }
